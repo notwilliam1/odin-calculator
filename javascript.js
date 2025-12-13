45a6f4340617682b9f154345a8c14c1874642772
William Causey <causeywilliam0@gmail.com>
2025-12-13 02:11:36 -0500

    Added functions to js
    Created 4 main functions add/sub/mult/div for calculator

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (a === 0 || b === 0) {
+        return 0;
+    }
+    return a / b;
+}
+
